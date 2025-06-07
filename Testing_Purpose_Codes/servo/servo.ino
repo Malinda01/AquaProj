c32fd4de061641a12024eb80ccadcf821517971e
Malinda Amarakoon <greshanamarakoon@gmail.com>
2025-06-07 22:51:18 +0530

    Update servo.ino

--- Testing_Purpose_Codes/servo/servo.ino
+++ Testing_Purpose_Codes/servo/servo.ino
@@ -1,11 +1,21 @@
 // Servo motor
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo myServo;
+
+void setup() {
+  myServo.attach(D5); // Use the GPIO pin you connected the signal to
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  myServo.write(0);    // Full speed in one direction
+  delay(2000);
+  myServo.write(90);   // Stop
+  delay(1000);
+  myServo.write(180);  // Full speed in the other direction
+  delay(2000);
+  myServo.write(90);   // Stop
+  delay(1000);
 }
+
